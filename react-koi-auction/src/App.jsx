e8dc535755cf156adfaffe3ecb34e0b54c1b0ffc
Eddyy1410 <eddynguyen14@gmail.com>
2024-09-23 17:20:40 +0700

    tạo mock api + register

--- react-koi-auction/src/App.jsx
+++ react-koi-auction/src/App.jsx
@@ -1,30 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './Header.jsx'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './Header/Header.jsx'
 import Home from './Home.jsx'
-import Login from './Login.jsx'
+import Login from './Login/Login.jsx'
 import About from './About.jsx'
-import Register from './Register.jsx'
+import Register from './Register/Register.jsx'
 import CurrentAuction from './CurrentAuction.jsx'
 import PastAuction from './PastAuction.jsx'
+import DemoAxios from './components/DemoAxios.jsx';
+import Footer from './Footer/Footer.jsx';
 
 
 function App() {
 
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/currentAuction" element={<CurrentAuction />} />
-        <Route path="/pastAuction" element={<PastAuction />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <div className='body'>
+        <Header />
+        <div className='body-content'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/currentAuction" element={<CurrentAuction />} />
+            <Route path="/pastAuction" element={<PastAuction />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </Router>
   )
 }
